ed0278f723a6e92f84bf1f462b50145bda880c23
alwinalbert <160037555+alwinalbert@users.noreply.github.com>
2025-05-31 14:19:46 +0530

    2nd commit

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,6 +1,6 @@
 import express from "express";
 import dotenv from "dotenv";
-
+import {connectDB} from "./lib/db.js";
 import authRoutes from "./routes/auth.route.js";
 
 const app = express();
@@ -11,5 +11,6 @@ const PORT = process.env.PORT;
 app.use("/api/auth", authRoutes);
 
 app.listen(5001, () => {
-  console.log("server is running on PORT 5001");
+  console.log("server is running on PORT:"+ PORT);
+  connectDB();
 });
