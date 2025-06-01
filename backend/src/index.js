285b3862962848099f3b594dabe153e57a9abad1
alwinalbert <160037555+alwinalbert@users.noreply.github.com>
2025-06-02 00:31:57 +0530

    5th commit

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,4 +1,5 @@
 import express from "express";
+import cors from "cors" 
 import dotenv from "dotenv";
 import {connectDB} from "./lib/db.js";
 import authRoutes from "./routes/auth.route.js";
@@ -12,6 +13,10 @@ dotenv.config();
 const PORT = process.env.PORT;
 app.use(express.json())
 app.use(cookieParser())
+app.use(cors({
+  origin:"http://localhost:5173",
+  credentials:true
+}))
 
 app.use("/api/auth", authRoutes);
 app.use("/api/message", messageRoutes);
---
+++ frontend/src/store/useAuthStore.js
@@ -0,0 +1,40 @@
+import {create} from "zustand"
+import { axiosInstanace } from "../lib/axios"
+import toast from "react-hot-toast"
+
+
+export const useAuthStore = create((set)=>({
+    authUser:null,
+    isSigningUp:false,
+    isLoggingIn:false,
+    isUpdatingProfile:false,
+    
+    isCheckingAuth:true,
+
+    checkAuth:async () => {
+        try {
+            const res = await axiosInstanace.get("/auth/check")
+
+            set({authUser:res.data})
+        } catch (error) {
+            set({authUser:null})
+            
+        }finally{
+            set({isCheckingAuth:false})
+        }
+    },
+
+    signup:async(data)=>{
+        set({isSigningUp:true})
+        try {
+            const res= await axiosInstanace.post("/auth/signup",data)
+            set({authUser:res.data})
+            toast.success("Account created successfully")
+        } catch (error) {
+            toast.error(error.response.data.message);
+         }finally{
+            set({isSigningUp: false})
+        }
+
+    }
+}))
